ad5eb5cd38ccf05530d035e3e69e3d33310a9b0e
Heath Hwang <heath.supergene@gmail.com>
2019-07-30 16:47:06 +0800

    change the version of swift from 4 to 5

--- PullToRefresh/UIScrollView+PullToRefresh.swift
+++ PullToRefresh/UIScrollView+PullToRefresh.swift
@@ -33,7 +33,7 @@ public extension UIScrollView {
         }
     }
     
-    public func addPullToRefresh(_ pullToRefresh: PullToRefresh, action: @escaping () -> ()) {
+    func addPullToRefresh(_ pullToRefresh: PullToRefresh, action: @escaping () -> ()) {
         pullToRefresh.scrollView = self
         pullToRefresh.action = action
         
@@ -57,7 +57,7 @@ public extension UIScrollView {
         view.frame = CGRect(x: 0, y: originY, width: frame.width, height: view.frame.height)
         
         addSubview(view)
-        sendSubview(toBack: view)
+        sendSubviewToBack(view)
     }
     
     func removePullToRefresh(at position: Position) {
